26f7d0b0eddfe722995f269945dd72092ebea7e9
Swastik Nath <swastiknath@outlook.com>
2020-03-26 19:41:06 +0530

    inital_commit

---
+++ basepack/contracts/MetaCoin.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.4.21 <0.7.0;
+import 'node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol'
+import "./ConvertLib.sol";
+
+// This is a simple example of a coin-like contract.
+// It is not standards compatible and cannot be expected to talk to other
+// coin/token contracts. If you want to create a standards-compliant
+// token, see: https://github.com/ConsenSys/Tokens. Cheers!
+
+contract MetaCoin is StandardToken{
+   
+}
